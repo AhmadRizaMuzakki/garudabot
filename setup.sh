2a169c9240969b239eb31b1fb8030495e3fd719c
AhmadRizaMuzakki <rizaahmad934@gmail.com>
2026-08-29 10:38:11 +0700

    memperbaiki setup instalasi

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -e
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 if ! command -v nvm >&2; then
     echo "error: please install nvm first"
+    exit 1
+fi
+
+if ! command -v python3 >/dev/null 2>&1 && ! command -v python >/dev/null 2>&1; then
+    echo "error: please install python 3 first (needed to build racero-blocks artifacts)"
     exit 1
 fi
 
@@ -28,6 +34,7 @@ cd ../racero-blocks
 
 nvm use
 npm install
+npm run build
 
 echo "info: setting up racero-vm"
 cd ../racero-vm
